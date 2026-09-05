bb660f33d45c29a557bd40e10564dae3eef9ba92
NGANAWARA <romaric.nganas@gmail.com>
2026-09-05 16:31:43 +0200

    CONCEPTION TERMINE / RESTE PAGE CONTACT A FINALISER

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav");
+
+if (menuToggle && nav) {
+  menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    menuToggle.classList.toggle("active");
+  });
+}
